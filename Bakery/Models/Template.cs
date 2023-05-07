34eee9c9439ebdd2a42f8d352264a9ff6fe68e13
Matt Melito <matt_melito@outlook.com>
2023-05-06 18:43:33 -0700

    Add initial Bakery and Bakery.Tests directories.

---
+++ Bakery/Models/Template.cs
@@ -0,0 +1,13 @@
+using System.Collections.Generic;
+
+namespace Bakery.Models
+{
+    public class Vendor
+    {
+        // private static List<Vendors> _instances = new List<Category> {};
+        // public string Name {get; set; }
+
+        // public int Id { get; }
+
+    }
+}
